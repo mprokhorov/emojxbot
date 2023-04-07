dc5c45a1e7cf46c9bfee30f1db77f3b88dc5c483
mikhail <imready.pro@gmail.com>
2023-04-07 21:55:24 +0300

    Add Redis

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@ import asyncio
 import logging
 
 from aiogram import Bot, Dispatcher
+from aiogram.fsm.storage.redis import Redis, RedisStorage
 from config_reader import config
 from handlers import new, split
 
@@ -11,8 +12,10 @@ async def main():
         level=logging.INFO,
         format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
     )
+    redis = Redis(decode_responses=True)
     bot = Bot(token=config.bot_token.get_secret_value())
-    dp = Dispatcher()
+    # dp = Dispatcher()
+    dp = Dispatcher(storage=RedisStorage(redis=redis))
     dp.include_routers(new.router, split.router)
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
