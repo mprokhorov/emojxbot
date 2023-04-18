156756765cf21fc69a4ed9e742d16f19e0e1a7dd
Mikhail <imready.pro@gmail.com>
2023-04-18 03:50:02 +0300

    Add .sh

--- scr.sh
+++ scr.sh
@@ -5,3 +5,5 @@ set -o errexit
 /opt/render/project/scr/.venv/bin/python -m pip install --upgrade pip
 
 pip install https://github.com/aiogram/aiogram/archive/refs/heads/dev-3.x.zip
+
+/opt/render/project/scr/.venv/bin/python
