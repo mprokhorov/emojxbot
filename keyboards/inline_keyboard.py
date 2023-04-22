2b3d568476faf54913e9fbc144f6cffa056ddb68
Mikhail <imready.pro@gmail.com>
2023-04-22 23:28:44 +0300

    Rename directories

--- keyboards/inline_keyboard.py
+++ keyboards/inline_keyboard.py
@@ -2,7 +2,7 @@ from aiogram.types import InlineKeyboardButton
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 
 
-def make_inline_keyboard(items: list[str]):
+def make_inline_keyboard(items: list[str]) -> InlineKeyboardBuilder:
     builder = InlineKeyboardBuilder()
     for item in items:
         builder.add(InlineKeyboardButton(text=item, callback_data=item))
