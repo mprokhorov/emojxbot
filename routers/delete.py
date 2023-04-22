5d00c9c013eba17fecda59c4ca681664c4ad4c9e
Mikhail <imready.pro@gmail.com>
2023-04-23 00:22:01 +0300

    Edit delete.py

--- routers/delete.py
+++ routers/delete.py
@@ -1,6 +1,7 @@
 from aiogram import Router, Bot, types
 from aiogram.filters.command import Command
 from aiogram.fsm.context import FSMContext
+from aiogram.methods import EditMessageText
 from aiogram.types import Message, ReplyKeyboardRemove
 
 from keyboards.inline_keyboard import make_inline_keyboard
@@ -19,10 +20,12 @@ async def cmd_split(message: Message, state: FSMContext):
         return
     await state.set_state(DeleteSet.choosing_set)
     builder = make_inline_keyboard(sets_list)
-    await message.answer(
+    delete_list_message = await message.answer(
         "Click on the set you want to delete:",
         reply_markup=builder.as_markup()
     )
+    await state.update_data(chat_id=delete_list_message.chat.id)
+    await state.update_data(message_id=delete_list_message.message_id)
 
 
 @router.callback_query(DeleteSet.choosing_set)
@@ -30,6 +33,7 @@ async def delete_set(callback: types.CallbackQuery, state: FSMContext, bot: Bot)
     data = await state.get_data()
     set_name = callback.data
     is_empty = data['is_empty']
+
     if await bot.delete_sticker_set(set_name):
         is_empty.pop(set_name)
         await state.update_data(is_empty=is_empty)
@@ -39,3 +43,18 @@ async def delete_set(callback: types.CallbackQuery, state: FSMContext, bot: Bot)
     else:
         await callback.message.reply(f'Emoji set {set_name} was not deleted.')
         await callback.answer()
+
+    data = await state.get_data()
+
+    if len(is_empty) == 0:
+        await EditMessageText(chat_id=data['chat_id'],
+                              message_id=data['message_id'],
+                              text="The list of emoji sets is empty.",
+                              reply_markup=make_inline_keyboard([]).as_markup())
+    else:
+        builder = make_inline_keyboard(is_empty.keys())
+        await EditMessageText(chat_id=data['chat_id'],
+                              message_id=data['message_id'],
+                              text="Click on the set you want to delete:",
+                              reply_markup=builder.as_markup()
+                              )
